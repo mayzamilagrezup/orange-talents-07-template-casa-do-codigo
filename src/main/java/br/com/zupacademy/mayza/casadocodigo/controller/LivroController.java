0da7b45d41756924c4ab4227cec1790107e01719
mayza.milagre <mayza.milagre@zup.com.br>
2021-07-27 16:23:18 -0300

    lista de livros

--- src/main/java/br/com/zupacademy/mayza/casadocodigo/controller/LivroController.java
+++ src/main/java/br/com/zupacademy/mayza/casadocodigo/controller/LivroController.java
@@ -1,20 +1,21 @@
 package br.com.zupacademy.mayza.casadocodigo.controller;
 
 import br.com.zupacademy.mayza.casadocodigo.dto.request.NovoLivroRequest;
+import br.com.zupacademy.mayza.casadocodigo.dto.response.ListaLivrosResponse;
 import br.com.zupacademy.mayza.casadocodigo.dto.response.NovoLivroResponse;
 import br.com.zupacademy.mayza.casadocodigo.modelo.Livro;
 import br.com.zupacademy.mayza.casadocodigo.repository.LivroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/livros")
@@ -32,4 +33,12 @@ public class LivroController {
         Livro livro = livroRepository.save(request.toLivro(manager));
         return ResponseEntity.ok(new NovoLivroResponse(livro));
     }
+
+    @GetMapping
+    public ResponseEntity<List<ListaLivrosResponse>> listarLivros() {
+        List<Livro> livros = livroRepository.findAll();
+        List<ListaLivrosResponse> livrosDTO = livros.stream().map(ListaLivrosResponse::new).collect(Collectors.toList());
+        return ResponseEntity.ok(livrosDTO);
+    }
+
 }
---
+++ src/main/java/br/com/zupacademy/mayza/casadocodigo/dto/response/ListaLivrosResponse.java
@@ -0,0 +1,22 @@
+package br.com.zupacademy.mayza.casadocodigo.dto.response;
+
+import br.com.zupacademy.mayza.casadocodigo.modelo.Livro;
+
+public class ListaLivrosResponse {
+
+    private Long id;
+    private String titulo;
+
+    public ListaLivrosResponse(Livro livro) {
+      this.id = livro.getId();
+      this.titulo = livro.getTitulo();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+}
